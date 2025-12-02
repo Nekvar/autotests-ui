3cecd143c000df58e3bdfd58e23c98cb9bb041bd
rns <rns@silentwings.ru>
2025-12-02 09:47:43 +0300

    Работа с авторизацией в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -3,7 +3,12 @@ from playwright.sync_api import sync_playwright, expect
 with sync_playwright() as playwright:
     # Открываем браузер и создаем новую страницу
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    #page = browser.new_page()
+
+    # Создание контекста
+    context = browser.new_context()
+    # Создание страницы
+    page = context.new_page()
 
     # Переходим на страницу входа
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
@@ -24,6 +29,19 @@ with sync_playwright() as playwright:
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
+    # Сохранение состояния браузера
+    context.storage_state(path = 'browser-state.json')
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    # Указываем файл с сохраненным состоянием
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+
+    page.wait_for_timeout(5000)
+
     # Проверяем наличие заголовка Dashboard
-    check_display_dashboard = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(check_display_dashboard).to_have_text('Dashboard')
+    # check_display_dashboard = page.get_by_test_id('dashboard-toolbar-title-text')
+    # expect(check_display_dashboard).to_have_text('Dashboard')
