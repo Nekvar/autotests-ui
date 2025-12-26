051f16f59f6c09b75bda5ba55866786401517e18
rns <rns@silentwings.ru>
2025-12-26 16:37:04 +0300

    Наращивание тестовой базы и рефакторинг

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,6 +1,8 @@
 import pytest
 from playwright.sync_api import Playwright, Page
 
+from pages.authentication.registration_page import RegistrationPage
+
 
 @pytest.fixture
 def chromium_page(playwright: Playwright) -> Page:  # type: ignore
@@ -9,42 +11,24 @@ def chromium_page(playwright: Playwright) -> Page:  # type: ignore
     browser.close()
 
 
-@pytest.fixture(scope='session')
-def initialize_browser_state(playwright: Playwright) -> Page:  # type: ignore
-    # Открываем браузер и создаем контекст
+@pytest.fixture(scope="session")
+def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
-
-    # Создание страницы
     page = context.new_page()
 
-    # Переходим на страницу входа
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    registration_page = RegistrationPage(page=page)
+    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
+    registration_page.click_registration_button()
 
-    # Заполняем поле Email
-    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill("user.name@gmail.com")
-
-    # Заполняем поле Username
-    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    username_input.fill("username")
-
-    # Заполняем поле Password
-    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill("password")
-
-    # Нажимаем на кнопку Registration
-    registration_button = page.get_by_test_id('registration-page-registration-button')
-    registration_button.click()
-
-    # Сохранение состояния браузера
-    context.storage_state(path='browser-state.json')
+    context.storage_state(path="browser-state.json")
     browser.close()
 
 
 @pytest.fixture
 def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:  # type: ignore
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state='browser-state.json')
+    context = browser.new_context(storage_state="browser-state.json")
     yield context.new_page()  # type: ignore
     browser.close()
