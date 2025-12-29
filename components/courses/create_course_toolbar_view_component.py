435181a769cd081b30c8a25e8bab5d1dd4cfd4f4
rns <rns@silentwings.ru>
2025-12-29 12:03:16 +0300

    Наращивание тестовой базы и рефакторинг. Практикуемся в написании автотестов с использованием Page Object, Page Component, Page Factory

--- components/courses/create_course_toolbar_view_component.py
+++ components/courses/create_course_toolbar_view_component.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import Page
 
+import re
 from components.base_component import BaseComponent
 from elements.text import Text
 from elements.button import Button
@@ -28,6 +29,8 @@ class CreateCourseToolbarViewComponent(BaseComponent):
 
     def click_create_course_button(self):
         self.create_course_button.click()
+        # Дополнительно проверим, что произошел редирект на правильную страницу
+        self.check_current_url(re.compile(".*/#/courses"))
 
     def check_disabled_create_course_button(self):
         self.create_course_button.check_disabled()
--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -40,3 +40,40 @@ class TestCourses:
         courses_list_page.course_view.check_visible(
             index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
         )
+
+    def test_edit_course(self, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
+        create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+        create_course_page.create_course_exercises_toolbar_view.check_visible()
+        create_course_page.check_visible_exercises_empty_view()
+
+        create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+        create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
+        create_course_page.create_course_form.fill(
+            title="Playwright",
+            estimated_time="2 weeks",
+            description="Playwright",
+            max_score="100",
+            min_score="10"
+        )
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.course_view.check_visible(
+            index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
+        )
+        # Переход в режим редактирования через меню карточки
+        courses_list_page.course_view.menu.click_edit(index=0)
+
+        create_course_page.create_course_form.fill(
+            title="Automation-engineer",
+            estimated_time="1 weeks",
+            description="Automation-engineer",
+            max_score="200",
+            min_score="50"
+        )
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.course_view.check_visible(
+            index=0, title="Automation-engineer", max_score="200", min_score="50", estimated_time="1 weeks"
+        )
