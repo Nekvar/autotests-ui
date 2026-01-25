1c1e2cd29f21cd22457e7784278f5ba8942b3caa
Nekvar <hriakov91@mail.ru>
2026-01-25 14:34:16 +0300

    Добавляем информацию о переменных окружения в Allure отчет

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Self
 
-from pydantic import EmailStr, FilePath, HttpUrl, DirectoryPath, BaseModel
+from pydantic import EmailStr, FilePath, HttpUrl, DirectoryPath
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -11,13 +11,17 @@ class Browser(str, Enum):
     CHROMIUM = "chromium"
 
 
-class TestUser(BaseModel):
+class TestUser(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="TEST_USER")
+
     email: EmailStr
     username: str
     password: str
 
 
-class TestData(BaseModel):
+class TestData(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="TEST_DATA")
+
     image_png_file: FilePath  # type: ignore
 
 
@@ -28,9 +32,6 @@ class Settings(BaseSettings):
         env_nested_delimiter=".",
     )
 
-    def get_base_url(self) -> str:
-        return f"{self.app_url}/"
-
     app_url: HttpUrl
     headless: bool
     browsers: list[Browser]
@@ -38,21 +39,28 @@ class Settings(BaseSettings):
     test_data: TestData
     videos_dir: DirectoryPath  # type: ignore
     tracing_dir: DirectoryPath  # type: ignore
+    allure_results_dir: DirectoryPath  # type: ignore
     browser_state_file: FilePath  # type: ignore
+
+    def get_base_url(self) -> str:
+        return f"{self.app_url}/"
 
     @classmethod
     def initialize(cls) -> Self:
         videos_dir = DirectoryPath("./videos")
         tracing_dir = DirectoryPath("./tracing")
+        allure_results_dir = DirectoryPath("./allure-results")
         browser_state_file = FilePath("browser-state.json")
 
         videos_dir.mkdir(exist_ok=True)
         tracing_dir.mkdir(exist_ok=True)
+        allure_results_dir.mkdir(exist_ok=True)
         browser_state_file.touch(exist_ok=True)
 
         return Settings(
             videos_dir=videos_dir,
             tracing_dir=tracing_dir,
+            allure_results_dir=allure_results_dir,
             browser_state_file=browser_state_file
         )  # type: ignore
 
